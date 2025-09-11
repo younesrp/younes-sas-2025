a5be18c19df4405570e6662589af7340e64f1999
younesmouqla <younesmouqla03@gmail.com>
2025-09-11 18:53:00 +0200

    hey

--- structs/challenge8.c
+++ structs/challenge8.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 struct cercle{
-    char couleur[20];
     float rayon;
     float aire;
 };
-struct cercle createcercle(char couleur[], float rayon){
+struct cercle calculcercle( float rayon){
     struct cercle c;
-    strcpy(c.couleur, couleur);
+
     c.rayon = rayon;
     c.aire = 3.14 * rayon * rayon;
     return c;
 }
 int main (){
-    struct cercle mycercle = createcercle("rouge", 5.0);
-    printf("couleur : %s\nrayon : %.2f\naire : %.2f\n", mycercle.couleur,mycercle.rayon,mycercle.aire);
+    struct cercle mycercle = calculcercle( 5.0);
+    printf("rayon : %.2f\naire : %.2f\n",mycercle.rayon,mycercle.aire);
     return 0;
     
 }
